74b6ae7e853059b9b9ed464a7d54240a2201716a
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-07-23 17:12:34 +0530

    starting the server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+import { app } from "./app.js";
 import databaseConnect from "./db/database.js";
 import dotenv from 'dotenv'
 
 dotenv.config()
 databaseConnect()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`SERVER IS RUNNING AT PORT ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log(error)
+})

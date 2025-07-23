073e5b277c6d8bbaaef5b649c58e19924f49bb3e
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-07-23 17:57:24 +0530

    some options

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from "cookie-parser";
 
-export const app = express()
+const app = express()
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials :true
+}))
+
+app.use(express.json({
+    limit : '16kb'
+}))
+
+// url encoder
+app.use(express.urlencoded({
+    extended : true,
+    limit : '16kb'
+}))
+
+
+// for storing files and folders
+app.use(express.static('public'))
+
+
+app.use(cookieParser())
+
+// (error, req, res, next)
+export {app}

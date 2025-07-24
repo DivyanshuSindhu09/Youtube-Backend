953bcdb972d1a856ce15dc3adc2ada9fd6dd41c3
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-07-24 22:56:07 +0530

    multer configuration

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,2 +1,17 @@
 import multer from "multer";
 
+// ! from multer's documentation
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+
+    cb(null, file.originalname)
+  }
+})
+
+export const upload = multer({ 
+    storage, 
+})

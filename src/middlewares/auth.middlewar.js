b31b3a43bb74aa6fad4ded83043f45ff9ecb19ae
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-07-29 16:01:40 +0530

    logout completed

--- src/middlewares/auth.middlewar.js
+++ src/middlewares/auth.middlewar.js
@@ -4,25 +4,29 @@ import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js" 
 
  export const verifyJWT = asyncHandler( async (req, res, next) => {
-    // ! Header -> Key - Authorization ; 
-    // ! Value -> Bearer <token>
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-
-    if(!token){
-        throw new ApiError(400, "Unauthorized Request")
+    try {
+        // ! Header -> Key - Authorization ; 
+        // ! Value -> Bearer <token>
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+        if(!token){
+            throw new ApiError(400, "Unauthorized Request")
+        }
+        // ? agar password theek hai
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken._id) //? humne model mein yahi field pass ki thi
+    
+        if(!user){
+            // TODO: more discussion
+            throw new ApiError(401, "Invalid Access Token")
+        }
+    
+        // adding new field to req 
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401, error.message || "Invalid Access Token")
     }
-    // ? agar password theek hai
-
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-
-    const user = await User.findById(decodedToken._id) //? humne model mein yahi field pass ki thi
-
-    if(!user){
-        // TODO: more discussion
-        throw new ApiError(401, "Invalid Access Token")
-    }
-
-    // adding new field to req 
-    req.user = user
-    next()
  } )
